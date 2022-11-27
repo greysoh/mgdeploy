a50cb1ae71a8c6bb6b9c12f8c2a9c4f48076e6a8
greysoh <greysoh@pm.me>
2022-11-27 15:39:31 -0500

    feature: Implements package extracting.

---
+++ src/00_extract.js
@@ -0,0 +1,25 @@
+const backupLocation = prompt("Where is your backup located?");
+const backupPath = Deno.env.get("TEMP") + "\\extBackupData\\";
+
+try {
+  await Deno.remove(backupPath, { recursive: true });
+} catch (e) {
+  if (!(e instanceof Deno.errors.NotFound)) throw e;
+}
+
+await Deno.mkdir(backupPath);
+
+try {
+  await Deno.copyFile(backupLocation, backupPath + "backup.zip");
+} catch (e) {
+  if (!(e instanceof Deno.errors.NotFound)) throw e;
+
+  throw "Your backup was not found";
+}
+
+await WindowsAPI.runBatch(`@echo off
+cd "${backupPath}"
+tar -xf backup.zip
+`);
+
+await Deno.remove(backupPath + "backup.zip");
